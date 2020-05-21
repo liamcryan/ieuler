3cf13240632dda160d921e1e96d662e2556a22ff
liamcryan <limecrayon@gmail.com>
2020-05-21 16:20:36 -0700

    pass_obj working with click tests :)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import json
 import os
 import tempfile
+from unittest.mock import MagicMock
 
 import pytest
 from click.testing import CliRunner
@@ -9,12 +10,45 @@ from ieuler.client import Client
 
 
 @pytest.fixture
-def default_client():
+def problems():
+    with open(os.path.join(os.path.dirname(__file__), '.problems'), 'rt') as f:
+        return json.load(f)
+
+
+@pytest.fixture
+def solved_problem_4():
+    return {
+        "ID": 4,
+        "code": {
+            "python3": {
+                "filecontent": "\"\"\"{\n    \"Description / Title\": \"Largest palindrome product\",\n    \"ID\": 4,\n    \"Problem\": \"<div class=\\\"problem_content\\\" role=\\\"problem\\\">\\n<p>A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 \\u00d7 99.</p>\\n<p>Find the largest palindrome made from the product of two 3-digit numbers.</p>\\n</div>\",\n    \"page_url\": \"https://projecteuler.net/archives\",\n    \"problem_url\": \"https://projecteuler.net/problem=4\"\n}\n\n\"\"\"\n\n\ndef answer():\n    \"\"\" Solve the problem here! Make sure to return the answer. \"\"\"\n    return 0\n\n\nif __name__ == \"__main__\":\n    \"\"\" Below is OK to leave alone \"\"\"\n    print(answer())\n",
+                "filename": "4.py",
+                "submission": None
+            }
+        },
+    }
+
+
+@pytest.fixture
+def default_client(problems, solved_problem_4):
     d = [tempfile.mkstemp() for _ in range(0, 4)]  # make 4 temp files
-    yield Client(cookies_filename=d[0][1],
-                 credentials_filename=d[1][1],
-                 problems_filename=d[2][1],
-                 default_language_filename=d[3][1])
+    client = Client(cookies_filename=d[0][1],
+                    credentials_filename=d[1][1],
+                    problems_filename=d[2][1],
+                    default_language_filename=d[3][1])
+    ping_ipe = client.ping_ipe
+    get_all_problems = client.get_all_problems
+    get_from_ipe = client.get_from_ipe
+
+    client.ping_ipe = MagicMock(return_value=None)
+    client.get_all_problems = MagicMock(return_value=problems)
+    client.get_from_ipe = MagicMock(return_value=[solved_problem_4])
+
+    yield client
+
+    client.ping_ipe = ping_ipe
+    client.get_all_problems = get_all_problems
+    client.get_from_ipe = get_from_ipe
     for _ in d:
         os.close(_[0])
         os.unlink(_[1])
@@ -26,15 +60,9 @@ def default_session(default_client):
         def __init__(self):
             self.client = default_client
 
-    return Session()
+    yield Session()
 
 
 @pytest.fixture
 def runner():
     return CliRunner()
-
-
-@pytest.fixture
-def problems():
-    with open(os.path.join(os.path.dirname(__file__), '.problems'), 'rt') as f:
-        return json.load(f)
--- tests/test_ieuler.py
+++ tests/test_ieuler.py
@@ -1,13 +1,15 @@
 import json
 
-from ieuler.cli import ilr
+from ieuler.cli import ilr, fetch
 
 
-def test_default_client(default_client):
+def test_default_client(default_client, problems):
     assert default_client.cookies_filename != '.cookies'
     assert default_client.credentials_filename != '.credentials'
     assert default_client.problems_filename != '.problems'
     assert default_client.default_language_filename != '.default_language'
+    assert default_client.ping_ipe() is None
+    assert default_client.get_all_problems() == problems
 
 
 def test_default_session(default_session, default_client):
@@ -26,7 +28,19 @@ def test_config(runner):
         "language": "python3",
         "server": {
             "host": "127.0.0.1",
-            "port": 5000
+            "port": 2718
         }
     }
 
+
+def test_fetch(runner, default_session):
+    result = runner.invoke(fetch, obj=default_session)
+
+    # assert fetched problems are in order
+    p = 1
+    for _ in default_session.client.problems:
+        assert _['ID'] == p
+        p += 1
+
+    # assert number 4 has code
+    assert 'code' in default_session.client.problems[3]
