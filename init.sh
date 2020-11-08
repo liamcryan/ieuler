bf579323620973bb2638dec48911b0cfdc17f9ab
liamcryan <limecrayon@gmail.com>
2020-11-07 21:12:23 -0800

    require credentials (really only username) in init.sh

--- init.sh
+++ init.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 # this script assumes positional arguments of the form:
 # entity{"key":"value"}
-# the current possible entities are: 'cookies'
-# therefore, there should only be $1 (for now assume $1 is cookies)
-
-# ttyd: (not tried yet)
-#   http://127.0.0.1:7681/?arg=cookies{%22PPS%22:123}&arg=default-language-template{%22language%22:%22python3%22}
+# the current possible entities are: 'cookies' & 'credentials'
+# therefore, there should only be $1 & $2 (for now assume $1 is cookies & $2 is credentials)
+# *** Note -> only username required in credentials - password not needed ***
 
 # zsh: (this works)
-#   ./init.sh "cookies{'PHPSESSID':'a81d3ed4c57c24596ecefab68de13b89'}"
+#   ./init.sh "cookies{'PHPSESSID':'a81d3ed4c57c24596ecefab68de13b89'}" "credentials{'username':'limecrayon','password':''}"
 
 if [ "$#" -lt 1 ]
 then
   ilr fetch --anonymous
 else
-  python3 -c "from ieuler.client import Client;c = Client();c.dump_cookies(${1#'cookies'})"
+  python3 -c "from ieuler.client import Client;c = Client();c.dump_cookies(${1#'cookies'});c.dump_credentials(${2#'credentials'})"
   ilr fetch
 fi
 ilr --help
